3a90f21fbd51375077614637c4d819be4d15084e
HuangXuSen <2293046230@qq.com>
2023-08-19 16:13:39 +0800

    Init Commit

---
+++ Blog_04_front/src/main/java/com/hxsstu/BlogApplication.java
@@ -0,0 +1,19 @@
+package com.hxsstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: BlogApplication
+ * Package: com.hxsstu
+ * Description:
+ *
+ * @Author HuangXuSen
+ * @Create 2023/8/19-16:09
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
